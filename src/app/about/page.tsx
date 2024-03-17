a385224efdc56d471d608e08715060f194d38981
ariesdimasy <ariesdimasy>
2024-03-17 14:25:39 +0700

    first init

---
+++ src/app/about/page.tsx
@@ -0,0 +1,11 @@
+export default function About() {
+  return (
+    <div>
+      <h1> About Page </h1>
+      <hr></hr>
+      <a href="/"> Home </a>
+      <hr></hr>
+      <p> about page </p>
+    </div>
+  );
+}
